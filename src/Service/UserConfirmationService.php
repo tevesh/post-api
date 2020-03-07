eba045cb49f484dcd1cda68c50842ad23f915e52
Francesco <francesco.landoni@softecspa.it>
2020-03-07 15:35:48 +0100

    Create custom log channel and inject as logger into service

--- src/Service/UserConfirmationService.php
+++ src/Service/UserConfirmationService.php
@@ -5,6 +5,7 @@
     use App\Exception\InvalidConfirmationTokenException;
     use App\Repository\UserRepository;
     use Doctrine\ORM\EntityManagerInterface;
+    use Psr\Log\LoggerInterface;
 
     /**
      * Class UserConfirmationService
@@ -22,15 +23,22 @@
         private $entityManager;
 
         /**
+         * @var LoggerInterface
+         */
+        private $logger;
+
+        /**
          * UserConfirmationService constructor.
          *
          * @param UserRepository $repository
          * @param EntityManagerInterface $entityManager
+         * @param LoggerInterface $logger
          */
-        public function __construct(UserRepository $repository, EntityManagerInterface $entityManager)
+        public function __construct(UserRepository $repository, EntityManagerInterface $entityManager, LoggerInterface $logger)
         {
             $this->repository = $repository;
             $this->entityManager = $entityManager;
+            $this->logger = $logger;
         }
 
         /**
@@ -40,15 +48,19 @@
          */
         public function confirmUser(string $confirmationToken): void
         {
+            $this->logger->debug('Fetching user by confirmation token');
             $user = $this->repository->findOneBy(['confirmationToken' => $confirmationToken]);
 
             if (!$user) {
+                $this->logger->debug('Fetching user by confirmation token not found');
                 throw new InvalidConfirmationTokenException(sprintf('Cannot find confirmation token %s', $confirmationToken));
             }
 
             $user->setEnabled(true);
             $user->setConfirmationToken(null);
             $this->entityManager->flush();
+
+            $this->logger->debug('Confirm user by confirmation token');
         }
 
     }
