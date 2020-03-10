637a5940b1b820e5cc32a98cf60c6fc95e4769a3
Francesco <francesco.landoni@softecspa.it>
2020-03-10 09:13:10 +0100

    Cover 100% code test of class AuthoredEntitySubscriber

--- tests/EventSubscriber/AuthoredEntitySubscriberTest.php
+++ tests/EventSubscriber/AuthoredEntitySubscriberTest.php
@@ -4,11 +4,13 @@
     
     use \ApiPlatform\Core\EventListener\EventPriorities;
     use \App\Entity\BlogPost;
+    use App\Entity\Comment;
     use \App\Entity\User;
     use \App\EventSubscriber\AuthoredEntitySubscriber;
     use \Exception;
     use \PHPUnit\Framework\MockObject\MockObject;
     use \PHPUnit\Framework\TestCase;
+    use Symfony\Component\HttpFoundation\Request;
     use \Symfony\Component\HttpKernel\Event\ViewEvent;
     use \Symfony\Component\HttpKernel\KernelEvents;
     use \Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
@@ -23,36 +25,47 @@
             $this->assertArrayHasKey(KernelEvents::VIEW, $result);
             $this->assertEquals(['getAuthenticateUser', EventPriorities::PRE_WRITE], $result[KernelEvents::VIEW]);
         }
-        
+    
         /**
+         * @param bool $hasToken
+         *
          * @return MockObject|TokenStorageInterface
          * @throws Exception
          */
-        public function getMockTokenInterface(): MockObject
+        public function getTokenStorageInterfaceMock(bool $hasToken = true): MockObject
         {
             $tokenMock = $this->getMockBuilder(TokenInterface::class)->getMockForAbstractClass();
             
             $tokenMock
-                ->expects($this->once())
+                ->expects($hasToken ? $this->once() : $this->never())
                 ->method('getUser')
                 ->willReturn(new User());
             
-            $tokenStorageMock = $this->getMockBuilder(TokenStorageInterface::class)->getMockForAbstractClass();
+            $tokenStorageInterfaceMock = $this->getMockBuilder(TokenStorageInterface::class)->getMockForAbstractClass();
             
-            $tokenStorageMock
+            $tokenStorageInterfaceMock
                 ->expects($this->once())
                 ->method('getToken')
-                ->willReturn($tokenMock);
+                ->willReturn($hasToken ? $tokenMock : null);
             
-            return $tokenStorageMock;
+            return $tokenStorageInterfaceMock;
         }
-    
+        
         /**
+         * @param string $method
+         * @param $controllerResult
+         *
          * @return MockObject|ViewEvent
-         * @throws Exception
          */
-        public function getMockViewEvent(): MockObject
+        public function getViewEventMock(string $method, $controllerResult): MockObject
         {
+            $requestMock = $this->getMockBuilder(Request::class)
+                ->getMock();
+            $requestMock
+                ->expects($this->once())
+                ->method('getMethod')
+                ->willReturn($method);
+            
             $viewEventMock = $this->getMockBuilder(ViewEvent::class)
                 ->disableOriginalConstructor()
                 ->getMock();
@@ -60,21 +73,80 @@
             $viewEventMock
                 ->expects($this->once())
                 ->method('getControllerResult')
-                ->willReturn(new BlogPost());
+                ->willReturn($controllerResult);
+            
+            $viewEventMock
+                ->expects($this->once())
+                ->method('getRequest')
+                ->willReturn($requestMock);
             
             return $viewEventMock;
         }
         
         /**
+         * @param string $concreteEntityClassName
+         * @param bool $shouldCallSetAuthorMethod
+         *
+         * @return MockObject
+         */
+        public function getEntityMock(string $concreteEntityClassName, bool $shouldCallSetAuthorMethod): MockObject
+        {
+            $entityMock = $this->getMockBuilder($concreteEntityClassName)
+                ->setMethods(['setAuthor'])
+                ->getMock();
+            $entityMock
+                ->expects($shouldCallSetAuthorMethod ? $this->once() : $this->never())
+                ->method('setAuthor');
+            
+            return $entityMock;
+        }
+    
+        /**
          * @throws Exception
          */
-        public function testSetAuthorCall(): void
+        public function testNoTokenPresent(): void
         {
-            $tokenStorageMock = $this->getMockTokenInterface();
-            
-            $viewEventMock = $this->getMockViewEvent();
-            
-            (new AuthoredEntitySubscriber($tokenStorageMock))->getAuthenticateUser($viewEventMock);
+            $tokenStorageInterfaceMock = $this->getTokenStorageInterfaceMock(false);
+    
+            $viewEventMock = $this->getViewEventMock(Request::METHOD_POST, new class {});
+    
+            (new AuthoredEntitySubscriber($tokenStorageInterfaceMock))->getAuthenticateUser($viewEventMock);
         }
         
+        /**
+         * @dataProvider providerSetAuthorCall
+         *
+         * @param string $concreteEntityClassName
+         * @param string $method
+         *
+         * @param bool $shouldCallSetAuthorMethod
+         *
+         * @throws Exception
+         */
+        public function testSetAuthorCall(string $concreteEntityClassName, string $method, bool $shouldCallSetAuthorMethod): void
+        {
+            $entityMock = $this->getEntityMock($concreteEntityClassName, $shouldCallSetAuthorMethod);
+            
+            $tokenStorageInterfaceMock = $this->getTokenStorageInterfaceMock();
+            
+            $viewEventMock = $this->getViewEventMock($method, $entityMock);
+            
+            (new AuthoredEntitySubscriber($tokenStorageInterfaceMock))->getAuthenticateUser($viewEventMock);
+        }
+    
+        /**
+         * Set up the arguments to pass to testSetAuthorCall and execute test for different scenarios
+         *
+         * @return array
+         */
+        public function providerSetAuthorCall(): array
+        {
+            return [
+                [BlogPost::class, Request::METHOD_POST, true],
+                [BlogPost::class, Request::METHOD_GET, false],
+                [Comment::class, Request::METHOD_POST, true],
+                [Comment::class, Request::METHOD_GET, false],
+                ['NonExisting', Request::METHOD_GET, false],
+            ];
+        }
     }
